417c732e2ad5ffdd4e66ae39c88a857e850e9810
duclongdev <20521566@gm.uit.edu.vn>
2023-12-13 14:10:16 +0700

    add Inventory model

---
+++ src/main/java/com/project/supermarket_be/domain/model/Inventory.java
@@ -0,0 +1,45 @@
+package com.project.supermarket_be.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "INVENTORY")
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_id")
+    private Long inventoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "create_by_staff", referencedColumnName = "id")
+    private Account createdByStaff;
+
+    @Column(name = "inventory_qnt")
+    private Integer inventoryQuantity;
+
+    @Column(name = "product_status", length = 20)
+    private String productStatus;
+
+    @Lob
+    @Column(name = "staff_signature")
+    private byte[] staffSignature;
+
+    @Column(name = "note", columnDefinition = "TEXT")
+    private String note;
+
+    @Column(name = "deleted_flag")
+    private boolean deletedFlag;
+
+}
