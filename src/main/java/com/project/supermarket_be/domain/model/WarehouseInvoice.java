9c4edef42fa7d84dce78ca959e6afa4a4f2fed4f
duclongdev <20521566@gm.uit.edu.vn>
2023-12-13 13:51:21 +0700

    add Warehouse Invoice

---
+++ src/main/java/com/project/supermarket_be/domain/model/WarehouseInvoice.java
@@ -0,0 +1,42 @@
+package com.project.supermarket_be.domain.model;
+
+import jakarta.persistence.*;
+import java.security.Timestamp;
+
+@Entity
+@Table(name = "WAREHOUSE_INVOICE")
+public class WarehouseInvoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "staff_id", referencedColumnName = "id")
+    private Account staff;
+
+    @Column(name = "receiving_time")
+    private Timestamp receivingTime;
+
+    @Column(name = "supply_code")
+    private String supplyCode;
+
+    @Column(name = "delivery_person_name")
+    private String deliveryPersonName;
+
+    @Column(name = "receiving_clerk_name")
+    private String receivingClerkName;
+
+    @Lob
+    @Column(name = "delivery_signature")
+    private byte[] deliverySignature;
+
+    @Lob
+    @Column(name = "receiving_signature")
+    private byte[] receivingSignature;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "deleted_flag")
+    private boolean deletedFlag;
+}
